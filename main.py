5f8651a594eaa2e58e5c88eaecd1738100d03266
samzolin <saz5193@psu.edu>
2020-09-18 18:07:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#Author: Samantha Zolin saz5193@psu.edu
+#Section: 10  
+
+def digit_sum(n):
+  if (n == 0):
+    return 0
+  else:
+    return n%10 + digit_sum(n//10)
+
+def run():
+  n = int(input("Enter an int: "))
+  print(f"sum of digits of {n} is {digit_sum(n)}")
+
+if __name__== "__main__":
+  run()
+
+
